45662cad21fc87c4e84dca24531ec3f9f7dae33d
“Mohamed <mohamedalymatar2001@gmail.com>
2023-09-30 03:30:55 +0200

    solved some bugs

--- rps.js
+++ rps.js
@@ -1,36 +1,37 @@
-console.log("Welcome to the console, stranger!");
-
-var op_names = ["Rain", "Mourra", "Azab", "Kymo", "Ayoub", "Sue", "Oreo", "Za3tar", "Ziad_Moraba3", "Eren_Yeager"]
+var op_names = ["Rain", "Mourra", "Azab", "Kymo", "Ayoub", "Sue", "Oreo", "Za3tar", "Ziad_Moraba3", "Eren_Yeager"];
 
 function typename() {
     var name = prompt("Enter your name: ", "Gon_Freecss");
     var playerName = document.getElementById("playerName");
-    (name == null) ? playerName.innerHTML += "Sakura <br>": playerName.innerHTML += name + "<br>";
+    if (name === null) {
+        playerName.innerHTML += "Sakura <br>";
+    } else {
+        playerName.innerHTML += name + "<br>";
+    }
 
     var oppoName = document.getElementById("oppoName");
     var opn = Math.random() * 9;
     oppoName.innerHTML += op_names[Math.round(opn)];
 }
 
-var choices = ["pics/rock.png", "pics/paper.png", "pics/scissors.png"]
-var values = ["rock", "paper", "scissors"]
+var choices = ["rock", "paper", "scissors"];
 
 function rock() {
     var player = document.getElementById("plc");
-    player.src = choices[0];
-    player.value = values[0];
+    player.src = "pics/rock.png";
+    player.value = "rock";
 }
 
 function paper() {
     var player = document.getElementById("plc");
-    player.src = choices[1];
-    player.value = values[1];
+    player.src = "pics/paper.png";
+    player.value = "paper";
 }
 
 function scissors() {
     var player = document.getElementById("plc");
-    player.src = choices[2];
-    player.value = values[2];
+    player.src = "pics/scissors.png";
+    player.value = "scissors";
 }
 
 function play() {
@@ -38,20 +39,25 @@ function play() {
     var computer = document.getElementById("clc");
 
     var com = Math.random() * 2;
-    computer.src = choices[Math.round(com)];
-    computer.value = values[Math.round(com)];
+    computer.src = "pics/" + choices[Math.round(com)] + ".png";
+    computer.value = choices[Math.round(com)];
 
     var score = document.getElementById("score");
+    // var scoreboard = document.getElementById("scoreboard");
 
     if (player.value == "scissors" && computer.value == "rock") {
-        score.innerHTML = "YOU LOST!!! :(";
+        score.innerHTML = "YOU LOST :(";
+        document.getElementById("scoreboard").style.background = "red";
     } else if (player.value == "paper" && computer.value == "scissors") {
-        score.innerHTML = "YOU LOST!!! :(";
+        score.innerHTML = "YOU LOST :(";
+        document.getElementById("scoreboard").style.background = "red";
     } else if (player.value == "rock" && computer.value == "paper") {
-        score.innerHTML = "YOU LOST!!! :(";
+        score.innerHTML = "YOU LOST :(";
+        document.getElementById("scoreboard").style.background = "red";
     } else if (player.value == computer.value) {
-        score.innerHTML = "A DRAW!!! XD";
+        score.innerHTML = "A TIE! XD";
     } else {
-        score.innerHTML = "YOU WON!!! :)";
+        score.innerHTML = "YOU WON :)";
+        document.getElementById("scoreboard").style.background = "green";
     }
 }
